e6b3892cab76efe056e6a7729e9c4e3f2f39f3db
Iisa Laasonen <iisa.laasonen@gmail.com>
2022-04-06 20:05:53 +0300

    endpoints

--- client/src/services/play.js
+++ client/src/services/play.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
 const hit = async () => {
-  const response = await axios.get("/hit");
-  return response;
+  const response = await axios.post("/hit");
+  return response.data;
 };
 
 const stand = async () => {
-  const response = await axios.get("/stand");
-  return response;
+  const response = await axios.post("/stand");
+  return response.data;
 };
 
-export { hit, stand };
+const newGame = async () => {
+  const response = await axios.get("/newGame");
+  return response.data;
+};
+
+export default { hit, stand, newGame };
