7fa0aea524c4b638db686b1e38e1e589bd311658
Iisa Laasonen <iisa.laasonen@gmail.com>
2022-04-06 15:02:54 +0300

    rules page start

--- client/src/components/Rules.js
+++ client/src/components/Rules.js
@@ -1,6 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const PageDiv = styled.div`
+  background: var(--color-background);
+  color: var(--color-text);
+  display: flex;
+  justify-content: center;
+  width: 80vw;
+  margin: 2rem auto;
+  section {
+    margin: 1rem;
+  }
+  h1 {
+    text-align: center;
+    margin-bottom: 10px;
+  }
+`;
 
 const Rules = () => {
-  return <h1>Rules</h1>;
+  return (
+    <PageDiv>
+      <section>
+        <h1>Blackjack rules</h1>
+        <p>The goal is to beat the dealer's hand without going over 21.</p>
+      </section>
+    </PageDiv>
+  );
 };
 export default Rules;
